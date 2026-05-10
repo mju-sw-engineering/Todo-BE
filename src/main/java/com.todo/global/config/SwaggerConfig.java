7787364f5eccbedee5cad99a45096295f07c6a15
yunjin1213 <yunjinkim25@mju.ac.kr>
2026-05-10 17:50:34 +0900

    fix: 배포 Swagger 서버 URL 수정
    배포 Swagger UI가 HTTP 서버 URL로 API 요청을 생성해 브라우저에서 차단되는 문제를 해결합니다.
    
    prod 프로필에서 HTTPS API 서버 URL을 기본값으로 제공하고, 설정값이 있을 때 OpenAPI servers 항목에 반영합니다.

--- src/main/java/com.todo/global/config/SwaggerConfig.java
+++ src/main/java/com.todo/global/config/SwaggerConfig.java
@@ -5,17 +5,22 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(@Value("${app.api-server-url:}") String apiServerUrl) {
         String jwtScheme = "bearerAuth";
 
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info()
                         .title("Todo API")
                         .description("Todo 애플리케이션 API 문서")
@@ -27,5 +32,11 @@ public class SwaggerConfig {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+
+        if (StringUtils.hasText(apiServerUrl)) {
+            openAPI.servers(List.of(new Server().url(apiServerUrl)));
+        }
+
+        return openAPI;
     }
 }
