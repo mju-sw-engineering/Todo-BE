395ac1be857daf3b81bfa67f94faa158b323c2f0
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2026-07-28 00:36:12 +0900

    fix: 이메일 발송을 비동기 처리로 분리 (#84)

--- src/main/java/com/todo/global/mail/event/MailOutboxEventListener.java
+++ src/main/java/com/todo/global/mail/event/MailOutboxEventListener.java
@@ -1,6 +1,6 @@
 package com.todo.global.mail.event;
 
-import com.todo.global.mail.service.MailOutboxService;
+import com.todo.global.mail.service.MailAsyncDispatcher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -8,21 +8,25 @@ import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 /**
- * 적재 트랜잭션 커밋 후 즉시 발송을 시도한다. 실패해도 폴러가 재시도하므로 예외는 로깅만 한다.
+ * 적재 트랜잭션 커밋 후 비동기 발송을 요청한다. 제출에 실패해도 폴러가 재시도하므로 예외는 로깅만 한다.
  */
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class MailOutboxEventListener {
 
-    private final MailOutboxService mailOutboxService;
+    private final MailAsyncDispatcher mailAsyncDispatcher;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onMailEnqueued(MailEnqueuedEvent event) {
         try {
-            mailOutboxService.dispatch(event.outboxId());
+            mailAsyncDispatcher.dispatch(event.outboxId());
         } catch (RuntimeException e) {
-            log.warn("메일 즉시 발송 실패, 폴러 재시도 예정. outboxId={}", event.outboxId(), e);
+            log.warn(
+                    "MAIL_ASYNC_SUBMIT_FAILED outboxId={}, exceptionType={}, 폴러 재시도 예정",
+                    event.outboxId(),
+                    e.getClass().getSimpleName()
+            );
         }
     }
 }
